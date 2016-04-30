23eec21a9c12e7f2c774c5353cb232d4313899b3
Jainam Shah <JainamShah1911@gmail.com>
2016-04-30 01:53:17 -0400

    Edited the Display messages for the stat.php dropdown

--- stat.php
+++ stat.php
@@ -16,11 +16,11 @@ include("connect.php");
   	 <div align="center" class="form-group">
 		<label for="type">Get Statistics Of:</label>
 		  <select class="form-control" id="stat" name="stat" onChange="call()">
-		  	<option value="0">------No Selection------</option>
-			<option value="1">Top 10 most frequent borrowers</option>
-			<option value="2">Top 10 most borrowed books in a branch</option>
-			<option value="3">The 10 most popular books of the year</option>
-			<option value="4">Average fine paid per reader</option>
+		  	<option value="0">------ No Selection ------</option>
+			<option value="1">Top 10 Most Frequent Borrowers</option>
+			<option value="2">Top 10 Most Borrowed Books In A Branch</option>
+			<option value="3">The 10 Most Popular Books Of The Year</option>
+			<option value="4">Average Fine Paid Per Reader</option>
 		  </select>
 		  <script>
 								function call() {
