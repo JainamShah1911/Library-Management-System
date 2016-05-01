0c1034d2992e5d3bcc7d2773beb73551b79ee5a6
Jainam Shah <JainamShah1911@gmail.com>
2016-05-01 14:50:26 -0400

    ohoooo perfectly printing Fine in Myholdings.php

--- Myholdings.php
+++ Myholdings.php
@@ -18,28 +18,46 @@ $rid=$_SESSION['rid'];
 include("connect.php"); 
 
 
-    $user_query=mysql_query("select RDTIME from borrows WHERE READERID='$rid'")or die(mysql_error());
-	while($row=mysql_fetch_array($user_query)){
-	$_SESSION['aid']=$row['RDTIME'];
-	$result = mysql_query("SELECT DATEDIFF((SELECT RDTIME FROM borrows WHERE BORNUMBER = 2), (SELECT BDTIME FROM borrows WHERE BORNUMBER = 2)) AS days");
-		if (!$result) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
-}
-$days = mysql_fetch_row($result);
-$_SESSION['days']=$days['0'];
-$days=$_SESSION['days'];
+   
+		
+	//$result = mysql_query("SELECT DATEDIFF((SELECT RDTIME FROM borrows WHERE BORNUMBER='$bor'), (SELECT BDTIME FROM borrows WHERE BORNUMBER='$bor')) AS days");
+		//if (!$result) {
+			//			echo 'Could not run query: ' . mysql_error();
+				//		exit;
+					//  }
+		//	$days = mysql_fetch_row($result);
+			//$_SESSION['days']=$days;
+			//echo $_SESSION['days'];
+			//echo $_SESSION['days'][0];
+			//CalculateFine();
 
-
-	}
+	
 	
 		
-		 function CalculateFine(){
-		 	
-			$days=$_SESSION['days'];
- 			
-			$fine = $days * 10;
-			echo "$"." ".$fine;
+		 function CalculateFine($bor){
+		  		$rid=$_SESSION['rid'];
+				 
+		 
+		  //$user_query=mysql_query("select RDTIME,BORNUMBER from borrows WHERE READERID='$rid'")or die(mysql_error());
+	
+			//while($row=mysql_fetch_array($user_query)){
+		 
+				//$bor = $row['BORNUMBER'];
+				//echo "<br>".$bor;
+				//echo "<br>SELECT DATEDIFF((SELECT RDTIME FROM borrows WHERE BORNUMBER='$bor'), (SELECT BDTIME FROM borrows WHERE BORNUMBER='$bor')) AS days";
+		 		$result = mysql_query("SELECT DATEDIFF((SELECT RDTIME FROM borrows WHERE BORNUMBER='$bor'), (SELECT BDTIME FROM borrows WHERE BORNUMBER='$bor')) AS days");
+				$days = mysql_fetch_row($result);
+				echo "$"." ".$days[0]*10;
+				//while($row=mysql_fetch_array($result)){
+					//	print_r ($row[]);
+				//}
+		// }
+		 }		 	
+			
+			
+			//$fine = $_SESSION['days'][0] * 10;
+			//echo "$"." ".$fine;
+			
 			
 			
  			/*$dueDate = new DateTime($_SESSION['aid']);
@@ -49,7 +67,7 @@ $days=$_SESSION['days'];
  			$fine = $lateinterval > 0 ? intval(floor($lateinterval)) * 1 : 0;
  			echo $fine;*/ 
 			
-	}
+	
 
  ?>
  
@@ -67,7 +85,7 @@ $days=$_SESSION['days'];
     </tr>
   </thead>
   <?php  
-    $user_query=mysql_query("select TITLE,PUBNAME,BDTIME,RDTIME from document,borrows,publisher WHERE document.DOCID=borrows.DOCID AND document.PUBLISHERID=publisher.PUBLISHERID AND bOrrows.READERID='$rid'")or die(mysql_error());
+    $user_query=mysql_query("select BORNUMBER,TITLE,PUBNAME,BDTIME,RDTIME from document,borrows,publisher WHERE document.DOCID=borrows.DOCID AND document.PUBLISHERID=publisher.PUBLISHERID AND bOrrows.READERID='$rid'")or die(mysql_error());
 	while($row=mysql_fetch_array($user_query)){
 	?>
   <tbody>
@@ -78,7 +96,7 @@ $days=$_SESSION['days'];
       <td><?php echo $row['TITLE']; ?></td>
 	  <td><?php echo $row['BDTIME']; ?></td>
 	  <td><?php echo $row['RDTIME']; ?></td>
-	  <td><?php CalculateFine(); ?></td>
+	  <td><?php CalculateFine($row['BORNUMBER']); ?></td>
     </tr>
 	<?php } ?>
     
